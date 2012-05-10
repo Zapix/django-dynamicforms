9420dcd41173382a9acbaba95b637da9e1078b2d
Aibulatov Alex <zap.aibulatov@gmail.com>
2012-05-11 01:51:16 +0600

    Adding base structure of model and admin

---
+++ dynamicforms/admin.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from django.contrib import admin
+
+from dynamicforms import models
+
+
+class DynamicFieldInline(admin.TabularInline):
+    model = models.DynamicField
+
+
+class DynamicFormAdmin(admin.ModelAdmin):
+    inlines = [DynamicFieldInline]
+
+admin.site.register(models.DynamicForm, DynamicFormAdmin)
+
--- dynamicforms/models.py
+++ dynamicforms/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+from django.utils.translation import ugettext_lazy as _
+
+from dynamicforms.utils import get_choices_field_list
+
+
+class DynamicForm(models.Model):
+    '''
+    Model for building forms.
+    '''
+    name = models.CharField(
+               max_length=30,
+               verbose_name=_('Form name'),
+               help_text=_('Only in English and in camel notation'),
+               unique=True)
+    description = models.TextField(verbose_name=_('Form description'),
+                                   blank=True, null=True)
+
+    class Meta:
+        verbose_name = _('Dynamic Form')
+        verbose_name_plural = _('Dynamic Forms')
+
+    def __unicode__(self):
+        return unicode(self.name)
+
+
+class DynamicField(models.Model):
+    '''
+    Describing field for Dynamic class
+    '''
+    form = models.ForeignKey(DynamicForm, related_name='fields')
+    field = models.CharField(max_length=250, verbose_name=_('Form field'),
+                             choices=get_choices_field_list())
+    field_name = models.CharField(
+                     max_length=250,
+                     verbose_name=_('Field name'),
+                     help_text=_('Only in English and in lowercase'))
+    label = models.CharField(max_length=250, verbose_name=_('Label'))
+
